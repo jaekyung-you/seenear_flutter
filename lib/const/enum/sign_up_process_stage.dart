57b7473121af23a6c6beeceba5779f79de731a97
jaekyungyou <youjk_@naver.com>
2023-08-21 21:55:35 +0900

    회원가입 단계 enum화, 공통 화면 구현

---
+++ lib/const/enum/sign_up_process_stage.dart
@@ -0,0 +1,44 @@
+enum SignUpProcessStage {
+  residence,
+  interest,
+  interestRegion,
+  nickname;
+
+  String get title {
+    switch (this) {
+      case SignUpProcessStage.residence:
+        return '🏠 거주 지역 선택';
+      case SignUpProcessStage.interest:
+        return '💜 관심사 선택';
+      case SignUpProcessStage.interestRegion:
+        return '💜 관심지역 선택';
+      case SignUpProcessStage.nickname:
+        return '닉네임 입력';
+    }
+  }
+
+  String get subtitle {
+    switch (this) {
+      case SignUpProcessStage.residence:
+        return '홍길동 님의\n현재 거주 지역은 어디인가요?';
+      case SignUpProcessStage.interest:
+        return '홍길동 님의\n평소 관심사는 무엇인가요?';
+      case SignUpProcessStage.interestRegion:
+        return '홍길동 님의\n평소 관심있는 지역은 어디인가요?';
+      case SignUpProcessStage.nickname:
+        return '사용하고 싶은\n닉네임을 입력해주세요.';
+    }
+  }
+
+  String? get desc {
+    switch (this) {
+      case SignUpProcessStage.interest:
+      case SignUpProcessStage.interestRegion:
+        return '최대 5개 선택 가능';
+      case SignUpProcessStage.nickname:
+        return '최대 15자, 선택 입력';
+      default:
+        return null;
+    }
+  }
+}
--- lib/presentation/sign_up/widget/sign_up_screen.dart
+++ lib/presentation/sign_up/widget/sign_up_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:seenear/const/design_system/base_button.dart';
+import 'package:seenear/const/design_system/seenear_color.dart';
 import 'package:seenear/presentation/sign_up/controller/sign_up_controller.dart';
 
 // GetPage에서 put했으니 GetView사용해도 될 듯 (이미 put되어야함, GetView는 find하는 wrapper)
@@ -8,6 +10,44 @@ class SignUpScreen extends GetView<SignUpController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text('헤더 영역'),
+            SingleChildScrollView(
+              child: Text('스크롤 영역'),
+            ),
+            buttonArea(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buttonArea() {
+    return Column(
+      children: [
+        BaseButton(buttonText: '선택 완료', isDisabled: false, onPressed: () {}),
+        InkWell(
+          onTap: () {},
+          child: Row(
+            children: [
+              Text(
+                '건너뛰기',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: SeenearColor.grey30),
+              ),
+              Image.asset(
+                'assets/images/arrow_right.png',
+                color: SeenearColor.grey30,
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
