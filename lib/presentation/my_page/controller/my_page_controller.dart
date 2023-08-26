385e42e96292c4bd5aeb320bcb28dc71953c3305
jaekyungyou <youjk96@gmail.com>
2023-08-27 01:42:14 +0900

    액션 후 스낵바 구현

--- lib/presentation/my_page/controller/my_page_controller.dart
+++ lib/presentation/my_page/controller/my_page_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:seenear/const/design_system/seenear_color.dart';
 import 'package:seenear/const/enum/my_page_menu.dart';
 import 'package:seenear/const/enum/my_page_setting.dart';
 
@@ -47,5 +48,30 @@ class MyPageController extends GetxController with GetSingleTickerProviderStateM
         },
       ),
     );
+  }
+
+  void _showSnackBar({required String title}) {
+    final getSnackBar = GetSnackBar(
+      dismissDirection: DismissDirection.none,
+      message: title,
+      messageText: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white),
+      ),
+      snackPosition: SnackPosition.BOTTOM,
+      borderRadius: 20,
+      margin: const EdgeInsets.all(10.0),
+      duration: const Duration(seconds: 2),
+      barBlur: 7.0,
+      onTap: (_) {
+        Get.back();
+      },
+      backgroundColor: SeenearColor.grey40,
+      snackStyle: SnackStyle.FLOATING,
+    );
+
+    final controller = SnackbarController(getSnackBar);
+    controller.show();
   }
 }
