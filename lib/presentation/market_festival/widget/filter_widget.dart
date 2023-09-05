62065bdae5fb52bea9bee5935eb3870bf93f0c86
jaekyungyou <youjk96@gmail.com>
2023-09-05 19:48:04 +0900

    전국장날 필터 및 리스트뷰 UI 구현

--- lib/presentation/market_festival/widget/filter_widget.dart
+++ lib/presentation/market_festival/widget/filter_widget.dart
@@ -1,10 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:seenear/const/design_system/seenear_color.dart';
 
 class FilterWidget extends StatelessWidget {
   const FilterWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          selectCell(title: '지역을 선택해주세요', onTap: () {}),
+          const SizedBox(
+            height: 10,
+          ),
+          selectCell(
+            title: '동네를 선택해주세요',
+            onTap: () {},
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          selectCell(
+            title: '방문 일자를 선택해주세요',
+            imageName: 'date_available',
+            onTap: () {},
+          ),
+          SizedBox(
+            height: 40,
+            child: Row(
+              children: [
+                selectedFilter(
+                  title: '인천',
+                  onTap: () {},
+                ),
+                selectedFilter(
+                  title: '남동구',
+                  onTap: () {},
+                ),
+                selectedFilter(
+                  title: '2023-10-07 (월)',
+                  onTap: () {},
+                ),
+                const Spacer(),
+                selectedFilter(
+                  title: '초기화',
+                  fgColor: SeenearColor.grey30,
+                  imageName: 'reset',
+                  onTap: () {},
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget selectCell({required String title, String? imageName, Function? onTap}) {
+    return InkWell(
+      onTap: () {
+        onTap!();
+      },
+      child: Container(
+        height: 40,
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6.0),
+          border: Border.all(
+            color: SeenearColor.grey20,
+            width: 1,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/images/${imageName ?? 'location'}.png',
+                  width: 22,
+                  color: SeenearColor.grey30,
+                ),
+                const SizedBox(
+                  width: 6,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, color: SeenearColor.grey50),
+                ),
+              ],
+            ),
+            Image.asset(
+              'assets/images/arrow_down.png',
+              width: 16,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget selectedFilter({
+    required String title,
+    Color? fgColor,
+    String? imageName,
+    Function? onTap,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: InkWell(
+        onTap: () {
+          onTap!();
+        },
+        child: Row(
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14, color: fgColor ?? SeenearColor.blue60),
+            ),
+            Image.asset(
+              'assets/images/${imageName ?? 'close'}.png',
+              color: fgColor ?? SeenearColor.blue60,
+              width: 16,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/market_festival/widget/market_festival_screen.dart
+++ lib/presentation/market_festival/widget/market_festival_screen.dart
@@ -6,6 +6,7 @@ import 'package:seenear/presentation/market_festival/widget/filter_widget.dart';
 import 'package:seenear/presentation/my_page/widget/my_page_menu/festival_cell.dart';
 import 'package:seenear/presentation/my_page/widget/my_page_menu/market_cell.dart';
 
+import '../../../const/design_system/seenear_color.dart';
 import '../controller/market_festival_controller.dart';
 
 class MarketFestivalScreen extends GetView<MarketFestivalController> {
@@ -19,10 +20,18 @@ class MarketFestivalScreen extends GetView<MarketFestivalController> {
       child: SafeArea(
         child: Column(
           children: [
-            BaseHeader(title: isFestival ? '축제/행사' : '전통시장'),
-            Expanded(child: Container(color: Colors.blue,),),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: BaseHeader(title: isFestival ? '축제/행사' : '전통시장'),
+            ),
+            FilterWidget(),
+            // Expanded(
+            //   child: Container(
+            //     color: Colors.blue,
+            //   ),
+            // ),
             // FilterWidget(),
-            // contentView(isFestival),
+            Expanded(child: contentView(isFestival)),
           ],
         ),
       ),
@@ -30,19 +39,95 @@ class MarketFestivalScreen extends GetView<MarketFestivalController> {
   }
 
   Widget contentView(bool isFestival) {
-    return isFestival
-        ? ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return FestivalCell(
-                onTapItemCell: () {},
-                onTapFavoriteIcon: () {},
-              );
-            })
-        : ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return MarketCell(onTapItemCell: () {}, onTapFavoriteIcon: () {});
-            });
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          color: const Color(0xFFF2F4F6),
+          height: 48,
+          child: Row(
+            children: [
+              Text(
+                '총 100개',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: SeenearColor.grey60,
+                ),
+              ),
+              const Spacer(),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  '거리순',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: SeenearColor.blue80,
+                  ),
+                ),
+              ),
+              Text(
+                ' | ',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: SeenearColor.grey40,
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  '인기순',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: SeenearColor.grey60,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(child: emptyView())
+        // isFestival
+        //     ? ListView.builder(
+        //         itemCount: 10,
+        //         itemBuilder: (context, index) {
+        //           return FestivalCell(
+        //             onTapItemCell: () {},
+        //             onTapFavoriteIcon: () {},
+        //           );
+        //         })
+        //     : ListView.builder(
+        //         itemCount: 10,
+        //         itemBuilder: (context, index) {
+        //           return MarketCell(onTapItemCell: () {}, onTapFavoriteIcon: () {});
+        //         })
+      ],
+    );
+  }
+
+  Widget emptyView() {
+    return Column(
+      children: [
+        const SizedBox(height: 80,),
+        Image.asset(
+          'assets/images/seenear_character_4.png',
+          width: 70,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          '선택하신 지역과 요일에\n열리는 장이 없어요',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: SeenearColor.grey30,
+          ),
+        ),
+      ],
+    );
   }
 }
