8fc127be0b0411381d4be904c87e44b02556c604
jaekyungyou <youjk96@gmail.com>
2023-09-10 00:38:27 +0900

    사진 접근 권한 sdk32 버전 이하 대응

--- lib/presentation/home/controller/home_controller.dart
+++ lib/presentation/home/controller/home_controller.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:seenear/util/image_picker.dart';
 import 'package:share_plus/share_plus.dart';
 import '../../../const/enum/home_menu.dart';
 import '../../../const/seenear_path.dart';
@@ -19,9 +18,7 @@ class HomeController extends GetxController {
         break;
       case HomeMenu.community:
         // 커뮤니티 진입, 화면 미기획
-      ImagePicker().showPermissionPopup();
-      // Share.share('check out my website https://example.com');
-      
+        Share.share('check out my website https://example.com');
         break;
       case HomeMenu.myInfo:
         // Get.toNamed(isLogined ? SeenearPath.MY_PAGE : SeenearPath.LOGIN);
--- lib/util/image_picker.dart
+++ lib/util/image_picker.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:seenear/const/design_system/base_popup.dart';
@@ -11,21 +14,48 @@ class ImagePicker {
     return _instance;
   }
 
-  Future<void> _checkPermission() async {
+  Future<bool> _checkPermission() async {
     // os 팝업 -> 설정 권한에 추가됨
     await Permission.photos.request();
 
-    PermissionStatus status = await Permission.photos.status;
+    PermissionStatus photoStatus = await Permission.photos.status;
+    PermissionStatus storageStatus = await Permission.storage.status;
 
-    if (status.isGranted) {
-      // todo: 포토 라이브러리 열기
-      return;
+    // ios는 사진 권한만 검사하고 끝
+    if (GetPlatform.isIOS) {
+      if (photoStatus.isGranted) {
+        return true;
+      }
+      await _showPermissionPopup();
+      return false;
     }
 
-    // todo: androidSdk 32 이하 추가 대응
+    if (GetPlatform.isAndroid) {
+      final androidInfo = await DeviceInfoPlugin().androidInfo;
+      // sdk 32 상위 버전이면 사진 권한 검사만 하고 끝
+      if (androidInfo.version.sdkInt > 32) {
+        if (photoStatus.isGranted) {
+          return true;
+        }
+        await _showPermissionPopup();
+        return false;
+      }
+      // sdk 32 이하면, storage 권한 체크도 해줘야 한다.
+      await Permission.storage.request();
+      storageStatus = await Permission.storage.status;
+
+      if (storageStatus.isGranted && photoStatus.isGranted) {
+        return true;
+      }
+      await _showPermissionPopup();
+      return false;
+    }
+
+    await _showPermissionPopup();
+    return false;
   }
 
-  void showPermissionPopup() {
+  Future<void> _showPermissionPopup() async {
     // todo: ios info.plist 설정
     // 1. App Transparency
     // 2. permission description
