d01892253878b2b4b7f477545887824c2dd206a6
jaekyungyou <youjk96@gmail.com>
2023-09-10 00:05:00 +0900

    insta_assets_picker 라이브러리 추가

---
+++ lib/util/image_picker.dart
@@ -0,0 +1,9 @@
+class ImagePicker {
+  ImagePicker._privateConstructor();
+
+  static final ImagePicker _instance = ImagePicker._privateConstructor();
+
+  factory ImagePicker() {
+    return _instance;
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import device_info_plus
 import package_info_plus
 import path_provider_foundation
+import photo_manager
 import share_plus
 import shared_preferences_foundation
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
