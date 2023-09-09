0644e0130fcca0a9448268bfe5133a15345cadd3
jaekyungyou <youjk96@gmail.com>
2023-09-10 00:13:03 +0900

    permission_handler 라이브러리 추가

--- lib/util/image_picker.dart
+++ lib/util/image_picker.dart
@@ -1,3 +1,8 @@
+
+
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 class ImagePicker {
   ImagePicker._privateConstructor();
 
@@ -5,5 +10,24 @@ class ImagePicker {
 
   factory ImagePicker() {
     return _instance;
+  }
+
+  Future<void> _checkPermission() async {
+    // os 팝업 -> 설정 권한에 추가됨
+    await Permission.photos.request();
+
+    PermissionStatus status = await Permission.photos.status;
+
+    if (status.isGranted) {
+      // todo: 포토 라이브러리 열기
+      return;
+    }
+
+    // todo: androidSdk 32 이하 추가 대응
+
+  }
+
+  void showPermissionPopup() {
+
   }
 }
