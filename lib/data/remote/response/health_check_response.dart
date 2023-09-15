c6fb478bc42e980767e969df6764b71243693beb
jaekyungyou <youjk96@gmail.com>
2023-09-15 19:52:42 +0900

    health 응답 fromJson 오류 수정

--- lib/data/remote/response/health_check_response.dart
+++ lib/data/remote/response/health_check_response.dart
@@ -3,21 +3,21 @@ import 'package:seenear/data/remote/response/member_response.dart';
 class HealthCheckResponse {
   String accessToken; // 발급받은 토큰
   bool member;
-  // MemberResponse memberView;
+  MemberResponse memberView;
 
-  HealthCheckResponse({required this.accessToken, required this.member,});
+  HealthCheckResponse({required this.accessToken, required this.member, required this.memberView});
 
   factory HealthCheckResponse.fromJson(Map<String, dynamic> json) {
     return HealthCheckResponse(
       accessToken: json['accessToken'],
       member: json['member'],
-      // memberView: json['memberView'].map((e) => MemberResponse.fromJson(e)),
+      memberView: MemberResponse.fromJson(json['memberView']),
     );
   }
 
   Map<String, dynamic> toJson() => {
         'accessToken': accessToken,
         'member': member,
-        // 'memberView': memberView.toJson(),
+        'memberView': memberView.toJson(),
       };
 }
