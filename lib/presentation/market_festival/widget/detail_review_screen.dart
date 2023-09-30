5a1c5d44e01f0549ad5cfec1b7460bf6bd854fe7
jaekyungyou <youjk96@gmail.com>
2023-09-30 19:11:54 +0900

    방문자 후기 상세화면 댓글창 UI 구현

--- lib/presentation/market_festival/widget/detail_review_screen.dart
+++ lib/presentation/market_festival/widget/detail_review_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:seenear/const/design_system/base_header.dart';
 import 'package:seenear/presentation/base_widget/seenear_base_scaffold.dart';
+import 'package:styled_text/styled_text.dart';
 import '../../../const/design_system/seenear_color.dart';
 import '../controller/detail_review_controller.dart';
 
@@ -230,10 +231,103 @@ class DetailReviewScreen extends GetView<DetailReviewController> {
   }
 
   Widget commentListView() {
-    return Center(
-      child: Container(
-        height: 500,
-        color: Colors.blue,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        StyledText(
+          text: '댓글<s>(2)</s>',
+          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18, color: SeenearColor.grey50),
+          tags: {
+            's': StyledTextTag(
+              style: const TextStyle(fontSize: 12),
+            ),
+          },
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        commentCell(isReplied: false),
+        commentCell(isReplied: true),
+        commentCell(isReplied: false),
+        commentCell(isReplied: false),
+        const SizedBox(
+          height: 16,
+        ),
+      ],
+    );
+  }
+
+  Widget commentCell({required bool isReplied}) {
+    String? profileImageSrc;
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (isReplied)
+            const SizedBox(
+              width: 32,
+            ),
+          profileImageSrc == null
+              ? Image.asset(
+                  'assets/images/default_profile.png',
+                  width: 36,
+                )
+              : CircleAvatar(
+                  radius: 18,
+                  backgroundImage: NetworkImage(
+                    profileImageSrc!,
+                  ),
+                ),
+          const SizedBox(
+            width: 8,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '오순도순세가족',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      '답글 달기',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: SeenearColor.blue60,
+                      ),
+                    )
+                  ],
+                ),
+                Text(
+                  '따님이랑 함께하는 모습 너무 보기 좋아요 따님이랑 함께하는 모습 너무 보기 좋아요 함께하는 모습 너무 보기 좋아요',
+                  maxLines: null,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  '2023-00-00 00:00:00',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                    color: SeenearColor.grey50,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
