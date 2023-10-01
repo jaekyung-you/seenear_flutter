e5fd41e41666b4c766067c4ed57dd3e86c239da9
jaekyungyou <youjk96@gmail.com>
2023-10-02 08:22:30 +0900

    검색 결과 화면 헤더 및 리스트 추가

--- lib/presentation/search/controller/search_controller.dart
+++ lib/presentation/search/controller/search_controller.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../data/remote/response/info_item_response.dart';
 
 class SearchScreenController extends GetxController with GetSingleTickerProviderStateMixin {
   // property
@@ -8,7 +9,7 @@ class SearchScreenController extends GetxController with GetSingleTickerProvider
   late TabController tabController;
   List<String> tabList = ['인기 검색어', '최근 검색어'];
   RxBool showResult = false.obs;
-
+  RxList<InfoItemResponse> searchResultList = <InfoItemResponse>[].obs;
 
   @override
   void onInit() {
@@ -16,6 +17,10 @@ class SearchScreenController extends GetxController with GetSingleTickerProvider
     tabController = TabController(vsync: this, length: 2);
     searchInputController.addListener(() {
       showResult.value = searchInputController.text.trim().isNotEmpty;
+      if (showResult.value) {
+        String keyword = searchInputController.text.trim();
+        _requestSearchResult(keyword: keyword);
+      }
     });
   }
 
@@ -23,5 +28,141 @@ class SearchScreenController extends GetxController with GetSingleTickerProvider
   void onClose() {
     tabController.dispose();
     super.onClose();
+  }
+
+  Future<void> _requestSearchResult({required String keyword}) async {
+    // todo: 임의로.. itemType이 달라지면 헤더를 넣어줌
+    // 1. api 호출
+
+    // 2. 성공하면 searchResultList에 넣기
+    // 임의로 mock data
+    InfoItemResponse mock1 = InfoItemResponse(
+      date: "2023-08-14",
+      id: 1,
+      imageSrc: "https://picsum.photos/id/37/200/300",
+      itemId: 101,
+      itemType: "FESTIVAL",
+      name: "Sample Festival 1",
+      reviewCount: 15,
+      score: 4,
+    );
+
+    InfoItemResponse mock2 = InfoItemResponse(
+      date: "2023-08-14",
+      id: 2,
+      imageSrc: "https://picsum.photos/id/27/200/300",
+      itemId: 101,
+      itemType: "FESTIVAL",
+      name: "Sample Festival 1",
+      reviewCount: 15,
+      score: 4,
+    );
+
+    InfoItemResponse mock3 = InfoItemResponse(
+      date: "2023-08-14",
+      id: 3,
+      imageSrc: "https://picsum.photos/id/23/200/300",
+      itemId: 101,
+      itemType: "MARKET",
+      name: "Sample MARKET 1",
+      reviewCount: 15,
+      score: 9,
+    );
+
+    InfoItemResponse mock4 = InfoItemResponse(
+      date: "2023-08-14",
+      id: 4,
+      imageSrc: "https://picsum.photos/id/7/200/300",
+      itemId: 101,
+      itemType: "MARKET",
+      name: "Sample MARKET 1",
+      reviewCount: 15,
+      score: 4,
+    );
+
+    InfoItemResponse mock5 = InfoItemResponse(
+      date: "2023-08-14",
+      id: 5,
+      imageSrc: "https://picsum.photos/id/3/200/300",
+      itemId: 101,
+      itemType: "CHAT",
+      name: "Sample ChAT 1",
+      reviewCount: 11,
+      score: 9,
+    );
+
+    InfoItemResponse chatHeader = InfoItemResponse(
+      date: "",
+      id: 0, // listview에서 id 중복되어도 되는가?
+      imageSrc: "",
+      itemId: 0,
+      itemType: "HEADER",
+      name: "이야기방",
+      reviewCount: 0,
+      score: 0,
+    );
+    InfoItemResponse marketHeader = InfoItemResponse(
+      date: "",
+      id: 0, // listview에서 id 중복되어도 되는가?
+      imageSrc: "",
+      itemId: 0,
+      itemType: "HEADER",
+      name: "전국 장날",
+      reviewCount: 0,
+      score: 0,
+    );
+    InfoItemResponse festivalHeader = InfoItemResponse(
+      date: "",
+      id: 0, // listview에서 id 중복되어도 되는가?
+      imageSrc: "",
+      itemId: 0,
+      itemType: "HEADER",
+      name: "축제/행사",
+      reviewCount: 0,
+      score: 0,
+    );
+
+    for (var element in [mock1, mock2, mock3, mock4, mock5]) {
+      searchResultList.add(element);
+      int index = searchResultList.indexOf(element);
+      int prevIndex = searchResultList.indexOf(element) - 1;
+
+      // 첫 번째 무조건 헤더 넣어줌
+      if (index == 0) {
+        if (element.itemType == "CHAT") {
+          searchResultList.insert(0, chatHeader);
+        }
+
+        if (element.itemType == "MARKET") {
+          searchResultList.insert(0, marketHeader);
+        }
+
+        if (element.itemType == "FESTIVAL") {
+          searchResultList.insert(0, festivalHeader);
+        }
+      } else if (prevIndex > 0 && searchResultList[index].itemType != searchResultList[prevIndex].itemType) {
+        // 이전과 현재의 itemType이 다르면, 현재 itemType  헤더 넣어줌
+        if (searchResultList[index].itemType == "CHAT") {
+          searchResultList.insert(index, chatHeader);
+        }
+
+        if (searchResultList[index].itemType == "MARKET") {
+          searchResultList.insert(index, marketHeader);
+        }
+
+        if (searchResultList[index].itemType == "FESTIVAL") {
+          searchResultList.insert(index, festivalHeader);
+        }
+      }
+
+
+
+
+
+    }
+
+    // 3. for-loop으로 헤더 넣기
+
+
   }
 }
--- lib/presentation/search/widget/search_result_screen.dart
+++ lib/presentation/search/widget/search_result_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:seenear/const/design_system/seenear_color.dart';
+import 'package:seenear/presentation/my_page/widget/my_page_menu/festival_cell.dart';
+import 'package:seenear/presentation/my_page/widget/my_page_menu/market_cell.dart';
+import 'package:seenear/presentation/my_page/widget/my_page_menu/review_cell.dart';
 import 'package:seenear/presentation/search/controller/search_controller.dart';
 
 class SearchResultScreen extends GetView<SearchScreenController> {
@@ -7,7 +11,37 @@ class SearchResultScreen extends GetView<SearchScreenController> {
 
   @override
   Widget build(BuildContext context) {
-    // todo: ListView로 ?!
-    return const Placeholder();
+    return ListView.builder(
+        itemCount: controller.searchResultList.length,
+        itemBuilder: (context, index) {
+          if (controller.searchResultList[index].itemType == "MARKET") {
+            return MarketCell(item: controller.searchResultList[index], onTapFavoriteIcon: () {}, onTapDelete: () {});
+          }
+
+          if (controller.searchResultList[index].itemType == "FESTIVAL") {
+            return FestivalCell(
+                item: controller.searchResultList[index], onTapItemCell: () {}, onTapFavoriteIcon: () {}, onTapDelete: () {});
+          }
+
+          // chat
+          if (controller.searchResultList[index].itemType == "MARKET") {
+            return ReviewCell(onTapItemCell: () {});
+          }
+
+          if (controller.searchResultList[index].itemType == "HEADER") {
+            return headerView(title: controller.searchResultList[index].name);
+          }
+        });
+  }
+
+  Widget headerView({required String title}) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 13),
+      color: SeenearColor.grey5,
+      child: Text(
+        title,
+        style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18, color: SeenearColor.grey60),
+      ),
+    );
   }
 }
