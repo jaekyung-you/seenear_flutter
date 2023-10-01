47f39a011db7ce529fe7af97097f7e3ba85ed676
jaekyungyou <youjk96@gmail.com>
2023-10-01 15:09:22 +0900

    bottom navigation bar UI 구현

--- lib/presentation/base_widget/seenear_base_scaffold.dart
+++ lib/presentation/base_widget/seenear_base_scaffold.dart
@@ -26,26 +26,74 @@ class _SeenearBaseScaffoldState extends State<SeenearBaseScaffold> {
           FocusScope.of(context).unfocus();
         },
         child: Scaffold(
-            bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              unselectedItemColor: SeenearColor.grey50,
-              selectedItemColor: SeenearColor.blue60,
-              selectedFontSize: 14,
-              unselectedFontSize: 14,
-              currentIndex: _selectedIndex,
-              items: [
-                BottomNavigationBarItem(label: '검색1',icon: Icon(Icons.search),),
-                BottomNavigationBarItem(label: '검색2',icon: Icon(Icons.search),),
-                BottomNavigationBarItem(label: '검색3',icon: Icon(Icons.search),),
-              ],
-              onTap: (index) {
-                // Get.offNamed해야할듯..?
-                setState(() {
-                  _selectedIndex = index;
-                });
-                Get.toNamed(SeenearPath.SEARCH);
-              },
-            ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            unselectedItemColor: SeenearColor.grey50,
+            selectedItemColor: SeenearColor.grey70,
+            selectedLabelStyle: TextStyle(fontWeight: FontWeight.w700, color: SeenearColor.grey70,),
+            unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w700, color: SeenearColor.grey50,),
+            selectedIconTheme: IconThemeData(color: SeenearColor.grey70,),
+            unselectedIconTheme: IconThemeData(color: SeenearColor.grey50,),
+            selectedFontSize: 14,
+            unselectedFontSize: 14,
+            currentIndex: _selectedIndex,
+            items: [
+              BottomNavigationBarItem(
+                label: '홈',
+                icon: Image.asset(
+                  'assets/images/tab_home${_selectedIndex == 0 ? '_selected': ''}.png',
+                  width: 28,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: '이야기방',
+                icon: Image.asset(
+                  'assets/images/tab_chat.png',
+                  width: 28,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: '검색',
+                icon: Image.asset(
+                  'assets/images/tab_search${_selectedIndex == 2 ? '_selected': ''}.png',
+                  width: 28,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: '장날',
+                icon: Image.asset(
+                  'assets/images/tab_market.png',
+                  width: 28,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: '축제',
+                icon: Image.asset(
+                  'assets/images/tab_smile.png',
+                  width: 28,
+                ),
+              ),
+            ],
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+              switch (index) {
+                case 0:
+                  Get.offAllNamed(SeenearPath.HOME);
+                case 1:
+                  break;
+                case 2:
+                  Get.offAllNamed(SeenearPath.SEARCH);
+                case 3:
+                  break;
+                case 4:
+                  break;
+                case 5:
+                  break;
+              }
+            },
+          ),
           body: SafeArea(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: widget.padding ?? 0.0),
