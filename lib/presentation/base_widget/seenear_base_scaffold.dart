d16c4e1d493963cf0baa12ace5e9ebeac052a4ad
jaekyungyou <youjk96@gmail.com>
2023-11-06 20:06:42 +0900

    탭 바 이미지 교체, 바텀 네비 살림

--- lib/presentation/base_widget/seenear_base_scaffold.dart
+++ lib/presentation/base_widget/seenear_base_scaffold.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:seenear/const/design_system/seenear_color.dart';
-import '../../const/seenear_path.dart';
 
 class SeenearBaseScaffold extends StatefulWidget {
   final Widget child;
@@ -15,7 +13,7 @@ class SeenearBaseScaffold extends StatefulWidget {
 }
 
 class _SeenearBaseScaffoldState extends State<SeenearBaseScaffold> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 2;
 
   @override
   Widget build(BuildContext context) {
@@ -27,74 +25,86 @@ class _SeenearBaseScaffoldState extends State<SeenearBaseScaffold> {
         },
         child: Scaffold(
           floatingActionButton: widget.floatingActionButton,
-          // bottomNavigationBar: BottomNavigationBar(
-          //   type: BottomNavigationBarType.fixed,
-          //   unselectedItemColor: SeenearColor.grey50,
-          //   selectedItemColor: SeenearColor.grey70,
-          //   selectedLabelStyle: TextStyle(fontWeight: FontWeight.w700, color: SeenearColor.grey70,),
-          //   unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w700, color: SeenearColor.grey50,),
-          //   selectedIconTheme: IconThemeData(color: SeenearColor.grey70,),
-          //   unselectedIconTheme: IconThemeData(color: SeenearColor.grey50,),
-          //   selectedFontSize: 14,
-          //   unselectedFontSize: 14,
-          //   currentIndex: _selectedIndex,
-          //   items: [
-          //     BottomNavigationBarItem(
-          //       label: '홈',
-          //       icon: Image.asset(
-          //         'assets/images/tab_home${_selectedIndex == 0 ? '_selected': ''}.png',
-          //         width: 28,
-          //       ),
-          //     ),
-          //     BottomNavigationBarItem(
-          //       label: '이야기방',
-          //       icon: Image.asset(
-          //         'assets/images/tab_chat.png',
-          //         width: 28,
-          //       ),
-          //     ),
-          //     BottomNavigationBarItem(
-          //       label: '검색',
-          //       icon: Image.asset(
-          //         'assets/images/tab_search${_selectedIndex == 2 ? '_selected': ''}.png',
-          //         width: 28,
-          //       ),
-          //     ),
-          //     BottomNavigationBarItem(
-          //       label: '장날',
-          //       icon: Image.asset(
-          //         'assets/images/tab_market.png',
-          //         width: 28,
-          //       ),
-          //     ),
-          //     BottomNavigationBarItem(
-          //       label: '축제',
-          //       icon: Image.asset(
-          //         'assets/images/tab_smile.png',
-          //         width: 28,
-          //       ),
-          //     ),
-          //   ],
-          //   onTap: (index) {
-          //     setState(() {
-          //       _selectedIndex = index;
-          //     });
-          //     switch (index) {
-          //       case 0:
-          //         Get.offAllNamed(SeenearPath.HOME);
-          //       case 1:
-          //         break;
-          //       case 2:
-          //         Get.offAllNamed(SeenearPath.SEARCH);
-          //       case 3:
-          //         break;
-          //       case 4:
-          //         break;
-          //       case 5:
-          //         break;
-          //     }
-          //   },
-          // ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            unselectedItemColor: SeenearColor.grey50,
+            selectedItemColor: SeenearColor.grey70,
+            selectedLabelStyle: TextStyle(
+              fontWeight: FontWeight.w700,
+              color: SeenearColor.grey70,
+            ),
+            unselectedLabelStyle: TextStyle(
+              fontWeight: FontWeight.w700,
+              color: SeenearColor.grey50,
+            ),
+            selectedIconTheme: IconThemeData(
+              color: SeenearColor.grey70,
+            ),
+            unselectedIconTheme: IconThemeData(
+              color: SeenearColor.grey50,
+            ),
+            selectedFontSize: 14,
+            unselectedFontSize: 14,
+            currentIndex: _selectedIndex,
+            items: [
+              BottomNavigationBarItem(
+                label: '장날',
+                icon: Image.asset(
+                  'assets/images/tab_market${_selectedIndex == 0 ? '_selected' : ''}.png',
+                  width: 28,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: '축제/행사',
+                icon: Image.asset(
+                  'assets/images/tab_festival${_selectedIndex == 1 ? '_selected' : ''}.png',
+                  width: 28,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: '홈',
+                icon: Image.asset(
+                  'assets/images/tab_home${_selectedIndex == 2 ? '_selected' : ''}.png',
+                  width: 28,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: '이야기방',
+                icon: Image.asset(
+                  'assets/images/tab_chat${_selectedIndex == 3 ? '_selected' : ''}.png',
+                  width: 28,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: '내 정보',
+                icon: Image.asset(
+                  'assets/images/tab_myInfo${_selectedIndex == 4 ? '_selected' : ''}.png',
+                  width: 28,
+                ),
+              ),
+            ],
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+              switch (index) {
+                case 0:
+                  // Get.offAllNamed(SeenearPath.HOME);
+                  break;
+                case 1:
+                  break;
+                case 2:
+                  // Get.offAllNamed(SeenearPath.SEARCH);
+                  break;
+                case 3:
+                  break;
+                case 4:
+                  break;
+                case 5:
+                  break;
+              }
+            },
+          ),
           body: SafeArea(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: widget.padding ?? 0.0),
